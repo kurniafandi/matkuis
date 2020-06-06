69a0d1137bfa331c2e69942cc240fc0df2a3072e
kurniafandi <57203843+kurniafandi@users.noreply.github.com>
2020-06-06 22:32:35 +0700

    Update and rename form.php to index.php

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ if (isset($_POST["mulai"])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Match Game</title>
+    <title>Math Game</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
 </head>
 <body>
